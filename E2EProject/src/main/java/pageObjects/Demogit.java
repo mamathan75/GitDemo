af66f03002f719820ab9bc184b167fab70b92509
Mamatha <nareddy0712@gmail.com>
2021-02-17 17:31:44 -0500

    This one is updated commit

---
+++ E2EProject/src/main/java/pageObjects/Demogit.java
@@ -0,0 +1,11 @@
+package pageObjects;
+
+public class Demogit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Git Repository");
+
+	}
+
+}
